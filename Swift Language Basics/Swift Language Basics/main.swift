aface85b8724b4a8fb3320cbc38b3029f51b70e3
Ryan Jones <raj8r8@mwc-054066.dhcp.missouri.edu>
2017-06-15 18:00:48 -0500

    First Push

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Ryan Jones on 6/15/17.
+//  Copyright © 2017 Ryan Jones. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
